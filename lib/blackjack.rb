c8b6947f423eab5ed8990f83a48d19e16bd4a808
jtang93 <tang.jonathan93@gmail.com>
2018-09-10 07:30:24 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,5 +64,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  initial_round
+  until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game
 end
     
